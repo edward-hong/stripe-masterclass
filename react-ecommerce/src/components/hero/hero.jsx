616fb6ad63fa94b2f895158592449ec57b107a3f
Edward Hong <edward.hong527@gmail.com>
2021-12-03 10:15:20 +1300

    S02L10 Hero

---
+++ react-ecommerce/src/components/hero/hero.jsx
@@ -0,0 +1,18 @@
+import './hero.styles.scss'
+
+const Hero = () => (
+	<section className="hero is-large is-info hero-image">
+		<div className="hero-body">
+			<div className="container">
+				<h1 className="hero-title">Bags reimagined for modern life.</h1>
+				<div className="shop-now-btn">
+					<button className="button is-black" id="show-now">
+						SHOW NOW
+					</button>
+				</div>
+			</div>
+		</div>
+	</section>
+)
+
+export default Hero
