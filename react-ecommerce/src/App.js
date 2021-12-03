92f18ee5dfca15ac1469e93386277c9e91f359bf
Edward Hong <edward.hong527@gmail.com>
2021-12-03 15:39:04 +1300

    S02L22 Single Product View 2

--- react-ecommerce/src/App.js
+++ react-ecommerce/src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom'
 import HomePage from './components/home-page'
 import NotFound from'./components/not-found'
 import Shop from './components/pages/shop/shop'
+import SingleProduct from './components/single-product/single-product'
 import './App.scss'
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
 			<Routes>
 				<Route exact path="/" element={<HomePage />} />
 				<Route path="/shop" element={<Shop />} />
+				<Route path="/product/:id" element={<SingleProduct />} />
 				<Route path="*" element={<NotFound />} />
 			</Routes>
 		</div>
--- react-ecommerce/src/components/single-product/single-product.jsx
+++ react-ecommerce/src/components/single-product/single-product.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useContext } from 'react'
-import { useParams, useNavigate, Navigate } from 'react-router'
+import { useParams, useNavigate } from 'react-router'
 
 import Layout from '../shared/layout'
 import { ProductsContext } from '../../context/products-context'
@@ -19,5 +19,46 @@ const SingleProduct = () => {
 		}
 
 		setProduct(product)
-	})
+	}, [products, id, navigate])
+
+	if (!product) {
+		return null
+	}
+
+	const { imageUrl, title, price, description } = product
+
+	return (
+		<Layout>
+			<div className="single=-product-container">
+				<div className="product-image">
+					<img src={imageUrl} alt="product" />
+				</div>
+				<div className="product-details">
+					<div className="name-proce">
+						<h3>{title}</h3>
+						<p>{price}</p>
+					</div>
+					<div clasName="add-to-cart-btns">
+						<button
+							className="button is-white nomad-btn"
+							id="btn-white-outline"
+						>
+							Add TO CART
+						</button>
+						<button
+							className="button is-black nomad-btn"
+							id="btn-white-outline"
+						>
+							PROCEED TO CHECKOUT
+						</button>
+					</div>
+					<div className="product-description">
+						<p>{description}</p>
+					</div>
+				</div>
+			</div>
+		</Layout>
+	)
 }
+
+export default SingleProduct
