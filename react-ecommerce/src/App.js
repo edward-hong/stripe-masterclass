78a322dde23268c7eeca18e6711d5738f57e720a
Edward Hong <edward.hong527@gmail.com>
2021-12-03 14:49:29 +1300

    S02L18 Not Found

--- react-ecommerce/src/App.js
+++ react-ecommerce/src/App.js
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 
 import HomePage from './components/home-page'
+import NotFound from'./components/not-found'
 import './App.scss'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
 		<div className="App">
 			<Routes>
 				<Route exact path="/" element={<HomePage />} />
+				<Route path="*" element={<NotFound />} />
 			</Routes>
 		</div>
 	)
