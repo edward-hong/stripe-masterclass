13dc78a1dff5fddf1767456e7d61d1043d11271e
Edward Hong <edward.hong527@gmail.com>
2021-12-03 14:56:52 +1300

    S02L19 Shop Page 1

--- react-ecommerce/src/App.js
+++ react-ecommerce/src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 
 import HomePage from './components/home-page'
 import NotFound from'./components/not-found'
+import Shop from './components/pages/shop/shop'
 import './App.scss'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
 		<div className="App">
 			<Routes>
 				<Route exact path="/" element={<HomePage />} />
+				<Route path="/shop" element={<Shop />} />
 				<Route path="*" element={<NotFound />} />
 			</Routes>
 		</div>
